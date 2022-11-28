c2dbee8057b1102d4f816eb1fc9368c29dab5b20
Muhibbul hasan <muhibbulhasan6@gmail.com>
2022-11-28 18:07:33 +0600

    blogs

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -2,8 +2,39 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-3'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">What are the different ways to manage a state in a React application?</h2>
+    <p>Local state is perhaps the easiest kind of state to manage in React, considering there are so many tools built into the core React library for managing it.
+
+useState is the first tool you should reach for to manage state in your components.
+
+It can take accept any valid data value, including primitive and object values. Additionally, its setter function can be passed down to other components as a callback function (without needing optimizations like useCallback).</p>
+    
+  </div>
+</div>
+<div className="card w-96 bg-base-100 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">How does prototypical inheritance work?</h2>
+    <p>The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object. getPrototypeOf and Object.</p>
+    
+  </div>
+</div>
+<div className="card w-96 bg-base-100 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">What is a unit test? Why should we write unit tests?</h2>
+    <p>The main objective of unit testing is to isolate written code to test and determine if it works as intended. Unit testing is an important step in the development process, because if done correctly, it can help detect early flaws in code which may be more difficult to find in later testing stages.</p>
+    
+  </div>
+</div>
+<div className="card w-96 bg-base-100 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">React vs. Angular vs. Vue?</h2>
+    <p>Vue provides higher customizability and hence is easier to learn than Angular or React. Further, Vue has an overlap with Angular and React with respect to their functionality like the use of components. Hence, the transition to Vue from either of the two is an easy option.</p>
+    
+  </div>
+</div>
         </div>
     );
 };
